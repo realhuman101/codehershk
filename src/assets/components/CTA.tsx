a7058a5519875c366d28a5ff948345baa5457755
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-11-06 20:50:11 +0800

    Remove old shenanigans

---
+++ src/assets/components/CTA.tsx
@@ -0,0 +1,9 @@
+function CTA() { // Call To Action
+  return (
+	<div id="CTA">
+		<h1 id="mainTitle">CodeHers</h1>
+	</div>
+  )
+}
+
+export default CTA
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Index from './pages/index.tsx'
-import Footer from './assets/components/Footer.tsx'
+import CTA from './assets/components/CTA';
 import './assets/css/main.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Index/>
-    <Footer/>
-  </StrictMode>,
+  <>
+    <CTA/>
+  </>,
 )
