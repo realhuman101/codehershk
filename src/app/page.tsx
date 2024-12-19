227632e9bc8a47ea989683709640ada18f756172
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-12-19 16:49:02 +0800

    shush typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
     <div className={styles.page}>
       <Navbar/>
 
+      {/* @ts-expect-error typescript tweaking rn */}
       <CustomCursor stiffness={1000} damping={25}/>
 
       {/* Hero section */}
