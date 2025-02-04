415c223bf196fa8d7410fde2c17d233bb48cb75a
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2025-02-04 22:01:11 +0800

    add more imgs

--- app/components/pics25.ts
+++ app/components/pics25.ts
@@ -12,4 +12,18 @@ const list = ['/event/audreySpeech.jpg',
 	'/event/panel.jpg',
 	'/event/workshop.jpeg']
 
+
+
+export const all = (splice: number = -1): Array<string> => {
+	const items =  [
+		'/event'
+	]
+
+	if (splice < 0 || splice > items.length)
+		return items
+	else
+		return items.splice(0, splice)
+}
+
 export default isProd ? list.map((item) => {return '/codehershk'+item}) : list
+
